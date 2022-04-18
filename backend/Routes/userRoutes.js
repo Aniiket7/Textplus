d88a9e8b5fafb79e24881ee2cf364e71c2807241
Aniiket7 <99549510+Aniiket7@users.noreply.github.com>
2022-04-18 15:25:44 +0530

    Delete backend directory

--- backend/Routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { protect } = require('../controllers/middlewares/authMIddleware');
-const { signup, login, allUsers } = require('../controllers/userController');
-const router = express.Router();
-
-router.route("/").post(signup).get(allUsers);
-router.post("/login", login)
-
-module.exports = router;
--- backend/config/genrateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-const genrateToken = (id)=>{
-    return jwt.sign({id},"aniket",{
-        expiresIn:"90d",
-    })
-}
-module.exports = genrateToken;
